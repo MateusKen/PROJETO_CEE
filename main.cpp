4e0482518eb05ea0d2701cf399b667750576e80b
Enrico258 <105428037+Enrico258@users.noreply.github.com>
2022-05-14 21:19:09 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-// colocar o código aqui
-
-// declarar variáveis
-int state1 = 0;
+int state1 = 0; 
 int state2 = 0;
 int dc1 = 5;
 int dc2 = 6;
 int push1 = 7;
 int push2 = 4;
 
-// declarar funções
-
- pinMode (dc1,OUTPUT);
+void setup()
+{
+ pinMode (dc1,OUTPUT); 
  pinMode (dc2,OUTPUT);
  pinMode (push1,INPUT);
  pinMode (push2,INPUT);
@@ -18,22 +15,22 @@ int push2 = 4;
 
 void loop()
 {
-  state1 = digitalRead (push1);
-  state2 = digitalRead (push2);
-  if (state1 == HIGH)
+  state1 = digitalRead (push1); //determina o stado 1 como o botão 1 está pressionado ou não
+  state2 = digitalRead (push2); //determina o stado 2 como o botão 2 está pressionado ou não
+  if (state1 == HIGH)//lê se o botão está pressionado
   {
-    digitalWrite (dc1,64);
+    digitalWrite (dc1,64);//liga o motor no sentido horário
     digitalWrite (dc2,LOW);
-    delay(3000);
-    digitalWrite (dc1,LOW);
+    delay(3000);//espera 3 segundos
+    digitalWrite (dc1,LOW);//desliga o motor
     digitalWrite (dc2,LOW);
   }
   if (state2 == HIGH)
   {
     digitalWrite (dc1,LOW);
-    digitalWrite (dc2,64);
-    delay(3000);
-    digitalWrite (dc1,LOW);
+    digitalWrite (dc2,64);//liga o motor no sentido anti-horário
+    delay(3000);//espera 3 segundos
+    digitalWrite (dc1,LOW);//desliga o motor
     digitalWrite (dc2,LOW);
   }
 }
