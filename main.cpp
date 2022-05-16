fbe182d08c7f15a91e12fb3b91d777eab2b7fd08
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-05-15 22:19:06 -0300

    Update main.cpp
    modifiquei o código anterior, criando duas funções, uma que sobe e outra que desce. Próximo passo: implementar mais um botão e implementar a lógica por trás.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 int state1 = 0; 
 int state2 = 0;
-int dc1 = 5;
-int dc2 = 6;
+int dc1 = 6;
+int dc2 = 5;
 int push1 = 7;
 int push2 = 4;
 
+void SubirAndar(){
+	 analogWrite (dc1,10);//liga o motor no sentido horário
+    analogWrite (dc2,LOW);
+    delay(3000);//espera 3 segundos
+    analogWrite (dc1,LOW);//desliga o motor
+    analogWrite (dc2,LOW);
+}
+
+void DescerAndar(){
+	analogWrite (dc1,LOW);
+    analogWrite (dc2,10);//liga o motor no sentido anti-horário
+    delay(3000);//espera 3 segundos
+    analogWrite (dc1,LOW);//desliga o motor
+    analogWrite (dc2,LOW);
+}
 void setup()
 {
  pinMode (dc1,OUTPUT); 
@@ -17,20 +32,13 @@ void loop()
 {
   state1 = digitalRead (push1); //determina o stado 1 como o botão 1 está pressionado ou não
   state2 = digitalRead (push2); //determina o stado 2 como o botão 2 está pressionado ou não
-  if (state1 == HIGH)//lê se o botão está pressionado
-  {
-    analogWrite (dc1,10);//liga o motor no sentido horário
-    analogWrite (dc2,LOW);
-    delay(3000);//espera 3 segundos
-    analogWrite (dc1,LOW);//desliga o motor
-    analogWrite (dc2,LOW);
+  if (state1 == HIGH){
+	SubirAndar();
   }
-  if (state2 == HIGH)
-  {
-    analogWrite (dc1,LOW);
-    analogWrite (dc2,10);//liga o motor no sentido anti-horário
-    delay(3000);//espera 3 segundos
-    analogWrite (dc1,LOW);//desliga o motor
-    analogWrite (dc2,LOW);
+  if (state2 == HIGH){
+  	DescerAndar();
   }
+
 }
+
+
