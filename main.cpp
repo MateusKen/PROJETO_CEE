424f32443e58af609d944dce99f75322e4b56fba
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-05-16 23:01:40 -0300

    Update main.cpp
    adicionei a lógica para o elevador subir e descer, coisas a adicionar: funções para as portas, lista de chamada

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-int state1 = 0; 
-int state2 = 0;
-int state3 = 0;
-int posicao = 3;
-int posicaoAtual = 0;
+int state1 = 0; // estado do botão 1
+int state2 = 0; // estado do botão 2
+int state3 = 0; // estado do botão 3
+int posicao = 3; //variável que indica a posicao atual do elevador
+int posicaoAtual = 0; // variavel que assosciada à posição, diz quantas vez o elevador repetirá a ação
+
 // OUTPUT
-int ElevadorMotor1 = 6;
+int ElevadorMotor1 = 6; 
 int ElevadorMotor2 = 5;
 
 /*
@@ -24,7 +25,7 @@ int BotaoAndar3= 2;
 
 
 // FUNÇÕES
-void SubirAndar(){
+void SubirAndar(){ // sobe o elevador 1 andar
 	 analogWrite (ElevadorMotor1,10);//liga o motor no sentido horário
     analogWrite (ElevadorMotor2,LOW);
     delay(3000);//espera 3 segundos
@@ -32,7 +33,7 @@ void SubirAndar(){
     analogWrite (ElevadorMotor2,LOW);
 }
 
-void DescerAndar(){
+void DescerAndar(){ // desce o elevador 1 andar
 	analogWrite (ElevadorMotor1,LOW);
     analogWrite (ElevadorMotor2,10);//liga o motor no sentido anti-horário
     delay(3000);//espera 3 segundos
@@ -68,7 +69,6 @@ void setup()
  pinMode (BotaoAndar1,INPUT);
  pinMode (BotaoAndar2, INPUT);
  pinMode(BotaoAndar3, INPUT);
- Serial.begin(9600);
 }
 
 void loop()
@@ -76,19 +76,44 @@ void loop()
   state1 = digitalRead (BotaoAndar1);                                                      
   state2 = digitalRead (BotaoAndar2);                                                        
   state3 = digitalRead (BotaoAndar3);
-  Serial.print(posicaoAtual);
-  Serial.println();
-  if (state1 == HIGH){
+  
+  if (state1 == HIGH){ //o que acontece ao apertar o botão do 1º andar
   	posicaoAtual = 1 - posicao;
-    Serial.write(posicaoAtual);
     if (posicaoAtual < 0){
-      abs(posicaoAtual);
-      for (posicaoAtual; posicaoAtual >0; posicaoAtual -=1){
-      	DescerAndar();
+      while (posicaoAtual <0){
+      	posicaoAtual += 1;
+        DescerAndar();
+      }
+    posicao = 1; 
+    }
+  }
+  
+  if (state2 == HIGH){ // o que acontece ao apertar o botão do 2º andar
+  	posicaoAtual = 2 - posicao;
+    if (posicaoAtual <0){
+      while (posicaoAtual < 0){
+      	posicaoAtual += 1;
+        DescerAndar();
+      }
+    }  
+    else{
+        while (posicaoAtual > 0){
+        	posicaoAtual -= 1;
+        	SubirAndar();
+        }	
+      }
+    posicao = 2;
+    }
+  if (state3 == HIGH){
+  	posicaoAtual = 3 - posicao;
+    if (posicaoAtual > 0){
+      while (posicaoAtual > 0){
+      	posicaoAtual -=1;
+        SubirAndar();
       }
     }
   }
-
 }
+
 
 
